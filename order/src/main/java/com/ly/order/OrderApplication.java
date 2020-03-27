b6d2bd33be4c2efe6fc32109d74b49c6425ce41a
LinHuanTan <ly.lin@yatsenglobal.com>
2020-03-27 16:13:02 +0800

    完成配置中心

---
+++ order/src/main/java/com/ly/order/OrderApplication.java
@@ -0,0 +1,12 @@
+package com.ly.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OrderApplication extends SpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
