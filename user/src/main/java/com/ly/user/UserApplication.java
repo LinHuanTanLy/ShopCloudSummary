ae082792f00fb22f853a9ed87b4276c490b13d75
LinHuanTan <ly.lin@yatsenglobal.com>
2020-03-27 16:04:12 +0800

    init 配置中心

--- user/src/main/java/com/ly/user/UserApplication.java
+++ user/src/main/java/com/ly/user/UserApplication.java
@@ -3,9 +3,11 @@ package com.ly.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.ly.user.mapper")
+@EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
